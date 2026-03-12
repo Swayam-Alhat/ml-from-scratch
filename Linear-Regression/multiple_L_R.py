6ccb95e308c5162b5c05669cd9c0c09f9385462d
Swayam-Alhat <swayamalhat@gmail.com>
2026-03-12 12:07:53 +0530

    data preprocessing completed

--- Linear-Regression/multiple_L_R.py
+++ Linear-Regression/multiple_L_R.py
@@ -13,10 +13,37 @@ df = df.sample(frac=1, random_state=42).reset_index(drop=True)
 is_null = df.isnull().any().any()
 print(is_null) # False
 
-# check if data is linear (i.e features have linear relationship with target)
-features = ["area","bedrooms"]
-for feature in features:
-    plt.scatter(df[feature],df["price"])
-    plt.xlabel(feature)
-    plt.ylabel("price")
-    plt.show()
+# EDA
+
+# check if house size have linear relationship with house price
+# plt.scatter(df["area"],df["price"])
+# plt.xlabel("house size")
+# plt.ylabel("house price")
+# plt.show()
+
+# correlation_coefficient = df["area"].corr(df["price"], method="pearson")
+# print(correlation_coefficient) # 0.53 this means Moderate linear relationship.
+
+# check how house price influences for no. of bedrooms
+# plt.bar(df["bedrooms"],df["price"])
+# plt.xlabel("bedrooms")
+# plt.ylabel("house price")
+# plt.show()
+
+# feature scaling - standardization
+df["area"] = (df["area"] - df["area"].mean()) / df["area"].std()
+df["bedrooms"] = (df["bedrooms"] - df["bedrooms"].mean()) / df["bedrooms"].std()
+
+# print(df["area"].mean())
+# print(df["bedrooms"].mean()) 
+# print(df["area"].std())
+# print(df["bedrooms"].std())
+
+# split data into training and testing
+train_df = df.iloc[0:431,:]
+test_df = df.iloc[431:,:]
+
+# actual implementation of algorithm 
+
+
+
