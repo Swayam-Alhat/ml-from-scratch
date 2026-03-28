a4684069eba19a950b120b1660e2c9912bb37b41
Swayam-Alhat <swayamalhat@gmail.com>
2026-03-29 00:11:43 +0530

    completed testing with accuracy test

--- Logistic-Regression/algorithm.py
+++ Logistic-Regression/algorithm.py
@@ -22,22 +22,30 @@ test_X = df.iloc[373:,:-1]
 test_Y = df.iloc[373:,-1]
 
 # feature scaling on training data
-train_X["Glucose"] = (train_X["Glucose"] - train_X["Glucose"].mean()) / train_X["Glucose"].std()
-train_X["BloodPressure"] = (train_X["BloodPressure"] - train_X["BloodPressure"].mean()) / train_X["BloodPressure"].std()
-train_X["SkinThickness"] = (train_X["SkinThickness"] - train_X["SkinThickness"].mean()) / train_X["SkinThickness"].std()
-train_X["Insulin"] = (train_X["Insulin"] - train_X["Insulin"].mean()) / train_X["Insulin"].std()
-train_X["Age"] = (train_X["Age"] - train_X["Age"].mean()) / train_X["Age"].std()
 
-# feature scaling on testing data
-# Note - we scale testing data using mean and std of training data
-test_X["Glucose"] = (test_X["Glucose"] - train_X["Glucose"].mean()) / train_X["Glucose"].std()
-test_X["BloodPressure"] = (test_X["BloodPressure"] - train_X["BloodPressure"].mean()) / train_X["BloodPressure"].std()
-test_X["SkinThickness"] = (test_X["SkinThickness"] - train_X["SkinThickness"].mean()) / train_X["SkinThickness"].std()
-test_X["Insulin"] = (test_X["Insulin"] - train_X["Insulin"].mean()) / train_X["Insulin"].std()
-test_X["Age"] = (test_X["Age"] - train_X["Age"].mean()) / train_X["Age"].std()
+# save mean and std of training data.
+# because we should not use mean and std of already scaled training data
+mean_x1 = train_X["Glucose"].mean()
+mean_x2 = train_X["BloodPressure"].mean()
+mean_x3 = train_X["SkinThickness"].mean()
+mean_x4 = train_X["Insulin"].mean()
+mean_x5 = train_X["Age"].mean()
+
+std_x1 = train_X["Glucose"].std()
+std_x2 = train_X["BloodPressure"].std()
+std_x3 = train_X["SkinThickness"].std()
+std_x4 = train_X["Insulin"].std()
+std_x5 = train_X["Age"].std()
+
+# feature scaling on training data
+train_X["Glucose"] = (train_X["Glucose"] - mean_x1) / std_x1
+train_X["BloodPressure"] = (train_X["BloodPressure"] - mean_x2) / std_x2
+train_X["SkinThickness"] = (train_X["SkinThickness"] - mean_x3) / std_x3
+train_X["Insulin"] = (train_X["Insulin"] - mean_x4) / std_x4
+train_X["Age"] = (train_X["Age"] - mean_x5) / std_x5 
 
 
-# Training
+# Implementation & Training
 epoch = 2000
 learning_rate = 0.01
 m1 = 0
@@ -80,6 +88,7 @@ for i in range(epoch):
     m5 = m5 - (learning_rate * grad_of_m5)
     b = b - (learning_rate) * grad_of_b
 
+# print last 5 values of loss_array
 print(loss_array[-6:])
 
 # plot graph of loss curve against iterations/epoch. so we know, how loss changed as iterations occured
@@ -92,3 +101,35 @@ plt.xlabel("Epochs")   # just labels the x axis, purely visual
 plt.ylabel("Loss")     # just labels the y axis, purely visual
 plt.title("Training Loss Curve")  # title of the graph, purely visual
 plt.show()             # actually renders and displays the graph
+
+# Testing
+
+# Before testing, we should scale input testing data using training data's mean and std
+test_X["Glucose"] = (test_X["Glucose"] - mean_x1) / std_x1
+test_X["BloodPressure"] = (test_X["BloodPressure"] - mean_x2) / std_x2
+test_X["SkinThickness"] = (test_X["SkinThickness"] - mean_x3) / std_x3
+test_X["Insulin"] = (test_X["Insulin"] - mean_x4) / std_x4
+test_X["Age"] = (test_X["Age"] - mean_x5) / std_x5
+
+z_test = (m1 * test_X["Glucose"]) + (m2 * test_X["BloodPressure"]) + (m3 * test_X["SkinThickness"]) + (m4 * test_X["Insulin"]) + (m5 * test_X["Age"]) + b
+
+prediction_test = 1 / (1 + np.exp(-z_test))
+
+prediction_test_binary = (prediction_test >= 0.5).astype(int)
+# prediction_test >= 0.5` — this compares each value in the series against 0.5. Returns a Series of True/Fals
+# 0.73  →  True
+# 0.31  →  False
+# 0.89  →  True
+# .astype(int)` — converts True/False to 1/0
+# True   →  1
+# False  →  0
+
+# Accuracy
+accuracy = (prediction_test_binary.values == test_Y.values).mean() * 100
+# prediction_test_binary.values == test_Y.values` — compares element by element, returns True where they match, False where they don't
+# predicted=1, actual=1  →  True
+# predicted=0, actual=1  →  False
+# predicted=0, actual=0  →  True
+# .mean() — this is the smart part. Mean of a True/False array treats True as 1 and False as 0. So if 75 out of 100 are True, mean = 0.75
+# * 100 — converts 0.75 to 75
+print(accuracy) # 81.98 % accuracy
