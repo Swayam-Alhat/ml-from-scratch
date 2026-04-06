8290e6d89ceb5f5916dd15ea3d0fdaf24b475c7d
Swayam-Alhat <swayamalhat@gmail.com>
2026-04-06 13:49:37 +0530

    calculated gini anf found splits

--- Decision-trees/algorithm.py
+++ Decision-trees/algorithm.py
@@ -11,31 +11,71 @@ df = df[df["Glucose"] != 0]
 df = df[df["BloodPressure"] != 0]
 df = df[df["SkinThickness"] != 0]
 
+# update index
+df.index = range(len(df))
+
 # df[df['Glucose'] != 0] — the inner part df['Glucose'] != 0 returns True/False for each row. Then df[...] keeps only the rows where it's True. So you're basically saying "give me only rows where Glucose is not zero.
 
 # training data
-train_X = df.iloc[:373, :-1]
-train_Y = df.iloc[:373,-1]
-
-# testing data
-test_X = df.iloc[373:,:-1]
-test_Y = df.iloc[373:,-1]
-
+training_data = df.iloc[:373, :]
 
 # implementation 
 
-# split feature in such a way that it creates two groups
-f1_unique_values = np.sort(train_X["Glucose"].unique()) 
-f2_unique_values = np.sort(train_X["BloodPressure"].unique()) 
-f3_unique_values = np.sort(train_X["SkinThickness"].unique())
-f4_unique_values = np.sort(train_X["Insulin"].unique())
-f5_unique_values = np.sort(train_X["Age"].unique())
+splits = []
+for i in range(training_data.shape[1] - 1):
+    unique_values = np.sort(training_data.iloc[:,i].unique())
+    split = (unique_values[:-1] + unique_values[1:]) / 2
+    splits.append(split)
 
-split_values_for_f1 = (f1_unique_values[:-1] + f1_unique_values[1:]) / 2
-split_values_for_f2 = (f2_unique_values[:-1] + f2_unique_values[1:]) / 2
-split_values_for_f3 = (f3_unique_values[:-1] + f3_unique_values[1:]) / 2
-split_values_for_f4 = (f4_unique_values[:-1] + f4_unique_values[1:]) / 2
-split_values_for_f5 = (f5_unique_values[:-1] + f5_unique_values[1:]) / 2
+# Now, splits contains array which contains split values for each feature. AT index 0, Glucose split values, At index 1, BloodPressure split values and so on
+
+
+gini = []
+
+index = 0
+for split in splits:
+
+    gini_of_column = []
+    for i in split:
+
+        # get left node values
+        left_node = training_data.loc[training_data.iloc[:,index] <= i, training_data.columns[-1]].to_numpy()
+        # get right node values
+        right_node = training_data.loc[training_data.iloc[:,index] > i, training_data.columns[-1]].to_numpy()
+
+        # defence if any of above node is empty
+        # this will add gini as 1. i.e highest gini. SO it will be not picked as good gini
+        if len(left_node) == 0 or len(right_node) == 0:
+            gini_of_column.append(1.0)
+            continue
+
+        # left node gini
+        left_node_gini = 1 - (((len(left_node[left_node == 0]) / len(left_node))**2) + ((len(left_node[left_node == 1]) / len(left_node))**2))
+
+        # right node gini
+        right_node_gini = 1 - (((len(right_node[right_node == 0]) / len(right_node))**2) + ((len(right_node[right_node == 1]) / len(right_node))**2))
+
+        # weighted gini
+        weighted_gini = ((len(left_node) / (len(left_node) + len(right_node))) * left_node_gini) + ((len(right_node) / (len(left_node) + len(right_node))) * right_node_gini)
+
+        gini_of_column.append(weighted_gini)
+    
+    index = index + 1
+    gini.append(gini_of_column)
+
+# we get gini array
+# Now find best gini, and then split
+
+best_splits = []
+
+for idx, val in enumerate(gini):
+
+    best_split = splits[idx][np.argmin(val)]
+    best_splits.append(best_split)
+
+print(best_splits)
+
+    
 
 
 
